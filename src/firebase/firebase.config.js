d6ffe67b117599469cbe60555f52cc7ec81f97fe
SamaulHaque <samaulhaque749@gmail.com>
2022-10-26 21:38:04 +0600

    added site name and title and favicon

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCg4x8JYOQ1OgEozBi_Ivy6lzzc68xnMjU",
+  authDomain: "bd-tech-f3e28.firebaseapp.com",
+  projectId: "bd-tech-f3e28",
+  storageBucket: "bd-tech-f3e28.appspot.com",
+  messagingSenderId: "23819815423",
+  appId: "1:23819815423:web:cc3e220538bf0a12a23fca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
